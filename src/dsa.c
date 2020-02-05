496470185b180a69c792777174cbe3c28d1586cf
Thiago Leucz Astrizi <thiago@bitbitbit.com.br>
2020-02-05 11:17:59 -0300

    Implementação  baseada no DSA implementada

--- src/dsa.c
+++ src/dsa.c
@@ -22,7 +22,7 @@ typedef struct{
 } MSG;
 
 typedef struct{
-  mpz_t rnd1, rnd2;
+  mpz_t r, s;
 } RND;
 
 typedef struct{
@@ -47,14 +47,14 @@ void init_digest(DIGEST *digest){
 }
 
 void init_rnd(RND *r){
-  mpz_init(r -> rnd1);
-  mpz_init(r -> rnd2);
+  mpz_init(r -> r);
+  mpz_init(r -> s);
 }
 
 
 void free_rnd(RND *r){
-  mpz_clear(r -> rnd1);
-  mpz_clear(r -> rnd2);
+  mpz_clear(r -> r);
+  mpz_clear(r -> s);
 }
 
 void free_digest(DIGEST *digest){
@@ -62,13 +62,15 @@ void free_digest(DIGEST *digest){
 }
 
 void print_keys(PK *pk, SK *sk){
-  char *string1;
+  char *string1, *string2;
   string1 = mpz_get_str(NULL, 10, sk -> x);
   printf("SK=(x = %s)\n", string1);
   free(string1);
   string1 = mpz_get_str(NULL, 10, pk -> y);
-  printf("PK=(y = %s)\n", string1);
+  string2 = mpz_get_str(NULL, 10, pk -> g);
+  printf("PK=(y = %s, g= %s)\n", string1, string2);
   free(string1);
+  free(string2);
 }
 
 void print_msg(MSG *m){
@@ -80,8 +82,8 @@ void print_msg(MSG *m){
 
 void print_hash(MSG *msg, RND *r, DIGEST *digest){
   char *string1, *string2, *string3;
-  string1 = mpz_get_str(NULL, 10, r -> rnd1);
-  string3 = mpz_get_str(NULL, 10, r -> rnd2);
+  string1 = mpz_get_str(NULL, 10, r -> r);
+  string3 = mpz_get_str(NULL, 10, r -> s);
   string2 = mpz_get_str(NULL, 10, digest -> rnd);
   printf("Hash(");
   print_msg(msg);
@@ -92,8 +94,8 @@ void print_hash(MSG *msg, RND *r, DIGEST *digest){
 }
 
 void random_rnd(PK *pk, RND *rnd){
-  mod_random_number(&(rnd -> rnd1), pk -> p);
-  mod_random_number(&(rnd -> rnd2), pk -> q);
+  mod_random_number(&(rnd -> r), pk -> p);
+  mod_random_number(&(rnd -> s), pk -> q);
 }
 
 void random_digest(PK *pk, DIGEST *digest){
@@ -126,9 +128,27 @@ void keygen(unsigned n, PK *pk, SK *sk){
   mpz_sub_ui(sk -> q, pk -> p, 1);
   mpz_divexact_ui(sk -> q, sk -> p, 2);
   mpz_set(pk -> q, sk -> q);
-  // Setting generator g:
-  mpz_set_ui(pk -> g, 2);
-  mpz_set_ui(sk -> g, 2);
+  // Choosing h random between 2 and p-2
+  {
+    mpz_t h, _p, aux;
+    mpz_init(h);
+    mpz_init(_p);
+    mpz_init(aux);
+    mpz_sub_ui(_p, sk -> p, 2);
+    do{
+      mod_random_number(&h, _p);
+      // Setting g:
+      mpz_sub_ui(_p, sk -> p, 1);
+      mpz_invert(aux, sk -> q, sk -> p);
+      mpz_mul(aux, aux, _p);
+      mpz_mod(aux, aux, sk -> p);
+      mpz_powm(pk -> g, h, aux, sk -> p);
+    } while(mpz_cmp_ui(pk -> g, 1) == 0);
+    mpz_set(sk -> g, pk -> g);
+    mpz_clear(h);
+    mpz_clear(_p);
+    mpz_clear(aux);
+  }
   // Random x mod q
   mod_random_number(&(sk -> x), sk -> q);
   // Setting y = g^x mod p:
@@ -136,35 +156,45 @@ void keygen(unsigned n, PK *pk, SK *sk){
 }
 
 void hash(PK *pk, MSG *msg, RND *rnd, DIGEST *digest){
-  mpz_t aux;
+  mpz_t aux, s_inv;
   mpz_init(aux);
-  // Computing y^e:
-  mpz_powm(digest -> rnd, pk -> y, msg -> msg, pk -> p);
-  // Computing g^r[1]
-  mpz_powm(aux, pk -> g, rnd -> rnd2, pk -> p);
-  // Multiplying the results, mod p
-  mpz_mul(digest -> rnd, digest -> rnd, aux);
+  mpz_init(s_inv);
+  // Computing aux = r/s mod q:
+  mpz_invert(s_inv, rnd -> s, pk -> q);
+  mpz_mul(aux, s_inv, rnd -> r);
+  mpz_mod(aux, aux, pk -> q);
+  // Computing digest = y^(r/s) mod p
+  mpz_powm(digest -> rnd, pk -> y, aux, pk -> p);
+  // Computing s_inv = m/s mod q
+  mpz_mul(s_inv, s_inv, msg -> msg);
+  mpz_mod(s_inv, s_inv, pk -> q);
+  // Computing s_inv = g^(m/s) mod p
+  mpz_powm(s_inv, pk -> g, s_inv, pk -> p);
+  // Computing digest = r - ((digest)(s_inv) mod p) mod q
+  mpz_mul(digest -> rnd, digest -> rnd, s_inv);
   mpz_mod(digest -> rnd, digest -> rnd, pk -> p);
-  // Making r[0] - result:
-  mpz_sub(digest -> rnd, rnd-> rnd1, digest -> rnd);
+  mpz_sub(digest -> rnd, rnd -> r, digest -> rnd);
   mpz_mod(digest -> rnd, digest -> rnd, pk -> q);
+  mpz_clear(s_inv);
   mpz_clear(aux);
 }
 
 void firstpreimage(SK *sk, MSG *msg, DIGEST *digest, RND *result){
-  //r2[0] = C + g^k mod p mod q
   mpz_t k;
   mpz_init(k);
   // Getting random k
   mod_random_number(&k, sk -> q);
-  mpz_powm(result -> rnd1, sk -> g, k, sk -> p);
-  mpz_add(result -> rnd1, result -> rnd1, digest -> rnd);
-  mpz_mod(result -> rnd1, result -> rnd1, sk -> p);
-  mpz_mod(result -> rnd1, result -> rnd1, sk -> q);
-  // r2[1] = k-(msg)x mod q
-  mpz_mul(result -> rnd2, msg -> msg, sk -> x);
-  mpz_sub(result -> rnd2, k, result -> rnd2);
-  mpz_mod(result -> rnd2, result -> rnd2, sk -> q);
+  // r = c + (g^k mod p) mod q
+  mpz_powm(result -> r, sk -> g, k, sk -> p);
+  mpz_add(result -> r, result -> r, digest -> rnd);
+  mpz_mod(result -> r, result -> r, sk -> q);
+  // s = (k^-1 (m+xr)) mod q
+  mpz_invert(k, k, sk -> q);
+  mpz_mul(result -> s, sk -> x, result -> r);
+  mpz_add(result -> s, result -> s, msg -> msg);
+  //mpz_mod(result -> s, result -> s, sk -> q);
+  mpz_mul(result -> s, result -> s, k);
+  mpz_mod(result -> s, result -> s, sk -> q);
   mpz_clear(k);
 }
 
